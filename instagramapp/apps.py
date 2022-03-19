20bd8c90d11a3a0b16d6de5142d42f9b805fac4f
moringa <tressy.mbone@student.moringaschool.com>
2022-03-19 19:50:45 +0300

    new folders

--- instagramapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class InstagramappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'instagramapp'
--- profiles/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def profile_view(request):
-    context={
-        'title':'profile'
-    }
-    return render(request,"profile.html",context)
